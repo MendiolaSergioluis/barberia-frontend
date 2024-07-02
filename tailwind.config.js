a00fa0a8abff343ad7aa6b68f7b3e69dafbf984c
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2024-07-02 12:12:58 -0400

    Modificaciones al Layout principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "app" : "url('img/Background.jpg')"
+      }
+    },
   },
   plugins: [],
 }
